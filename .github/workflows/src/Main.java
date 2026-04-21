08776ed11a61e4bada004a627a160182986ef016
Noor07-lab <shaiknoorahamed07@gmail.com>
2026-04-21 16:27:59 +0530

    updated java file

--- .github/workflows/src/Main.java
+++ .github/workflows/src/Main.java
@@ -1,5 +1,14 @@
-class Test
-  {
-    static int a=90;
-    helloo noor07
-  }
+// Java Program to demonstrate 
+// String to int conversion using parseInt()
+public class Main {
+    public static void main(String[] args) {
+      
+        String s = "12345"; 
+
+        // Convert the string to an integer 
+        // using Integer.parseInt()
+        int n = Integer.parseInt(s);
+
+        System.out.println("" + n);
+    }
+}
